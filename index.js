85e7feb7b07991f132bb6aaca5e476fc7163f001
Grady DeReamer <gradydereamer@Gradys-iMac.local>
2019-11-22 16:48:29 -0500

    ne# with '#' will be ignored, and an empty message aborts the commit.

--- index.js
+++ index.js
@@ -10,3 +10,11 @@
     <option value={currentItem}>{currentItem}</option>
   });
  */
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'returns'];
+let columnSelector = <option value="currentItem">currentItem</option>;
+<option value="currentItem">currentItem</option>
+letters.map((currentItem)=>{
+  <option value={currentItem}>{currentItem}</option>
+});
+
+var element = document.getElementById("columnSelector");
